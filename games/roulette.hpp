84327f313ac7c0d3715a79a15ea7aa2e6bdcdb89
rpehkone <pehkonenroni@gmail.com>
2023-02-14 19:20:15 +0200

    roulette clean place bets sys

--- games/roulette.hpp
+++ games/roulette.hpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <stack>
 #include <time.h>
+#include <iostream>
 
 class RouletteBet {
 	float amount;
@@ -25,27 +26,24 @@ class Roulette {
 		void spin()
 		{
 			spin_res = rand() % 37;
-			std::cout << "result: " << spin_res << "\n";
+			std::cout << "got number: " << spin_res << "\n";
 			check_results();
 		}
 
-		// void place_bet_green(float amount);
-		// void place_bet_single(int number, float amount);
-		// void place_bet_split();
 
-		void place_bet_1st_half(float amount)
-		{
-			if (!isBetAllowed(amount))
-			{
-				std::cout << "bet not allowed\n";
-				return;
-			}
-			bets_1st_half.push(amount);
-		}
-		// void place_bet_2nd_half(float amount);
-		// void place_bet_1st_third(float amount);
-		// void place_bet_2nd_third(float amount);
-		// void place_bet_3rd_third(float amount);
+		// 0 and singles return x35
+		// void place_bet_single(int number, float amount);
+		// void place_bet_even();
+		// void place_bet_odd();
+		// void place_bet_red();
+		// void place_bet_black();
+		// red = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+
+		void place_bet_1st_half(float amount) {place_bet(bets_1st_half, amount);};
+		void place_bet_2nd_half(float amount) {place_bet(bets_2nd_half, amount);};
+		void place_bet_1st_third(float amount) {place_bet(bets_1st_third, amount);};
+		void place_bet_2nd_third(float amount) {place_bet(bets_2nd_third, amount);};
+		void place_bet_3rd_third(float amount) {place_bet(bets_3rd_third, amount);};
 
 	private:
 		int get_player_id;
@@ -59,11 +57,17 @@ class Roulette {
 		std::stack<float> bets_2nd_third;
 		std::stack<float> bets_3rd_third;
 
-		bool isBetAllowed(float bet)
+		void place_bet(std::stack<float>& bets, float betAmount)
 		{
 			//substract from player
 			//check player total bets
-			return (bool)(bet >= minimum_bet && bet <= maximum_bet);
+			bool isBetAllowed = (bool)(betAmount >= minimum_bet && betAmount <= maximum_bet);
+			if (!isBetAllowed)
+			{
+				std::cout << "bet not allowed\n";
+				return;
+			}
+			bets.push(betAmount);
 		}
 
 		void check_results()
--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main()
 	Roulette ro(10, 10000);
 	ro.place_bet_1st_half(10);
 	ro.place_bet_1st_half(20);
-	ro.place_bet_1st_half(80);
+	ro.place_bet_2nd_half(80);
+	ro.place_bet_1st_third(20);
 	ro.spin();
 	return 0;
 }
